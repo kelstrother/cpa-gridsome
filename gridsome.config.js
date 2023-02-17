f8ee419129eceb9f12e000b9b7f17c7b96c7942b
Kel Strother <kelstrother@gmail.com>
2023-02-16 18:32:53 -0700

    removed toml file

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,27 +4,27 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = {
-  siteName: 'Strother CPA - Beyond the Numbers',
-  siteDescription: 'Strother CPA - Trusted CPA Services for Businesses and Individuals. Get expert tax advice, accounting solutions, and financial planning services to help you achieve your goals. Contact us today to set up an appointment.',
-  icon: './src/favicon.png',
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
-  },
-  plugins: [
-    {
-      use: 'gridsome-plugin-seo',
+module.exports =
+  {
+    siteName: "Strother CPA - Beyond the Numbers",
+    siteDescription:
+      "Strother CPA - Trusted CPA Services for Businesses and Individuals. Get expert tax advice, accounting solutions, and financial planning services to help you achieve your goals. Contact us today to set up an appointment.",
+    icon: "./src/favicon.png",
+    chainWebpack: (config) => {
+      const svgRule = config.module.rule("svg");
+      svgRule.uses.clear();
+      svgRule.use("vue-svg-loader").loader("vue-svg-loader");
     },
-  ],
-}
-
-// {
-//   use: '@gridsome/plugin-sitemap',
-//   options: {
-//     exclude: ['/privacy', '/legal']
-//   }
-// }
+    plugins: [
+      {
+        use: "gridsome-plugin-seo",
+      },
+    ],
+  } 
+  
+    // {
+    //   use: '@gridsome/plugin-sitemap',
+    //   options: {
+    //     exclude: ['/privacy', '/legal']
+    //   }
+    // }
