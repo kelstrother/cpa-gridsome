5681952f63e11c91e3f15c1c2f3e2e558669d75e
Kel Strother <kelstrother@gmail.com>
2023-03-01 14:15:00 -0700

    replaced Vue.use(aos) with client process.

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ export default function(Vue, { router, head, isClient }) {
     href:
     "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Spectral:wght@200;300;400;500;600;700;800&display=swap",
   });
-  Vue.use(AOS.init());
   if (process.isClient) {
     AOS.init()
   }
