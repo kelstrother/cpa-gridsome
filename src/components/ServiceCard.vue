1ea59c48f2d78b87704167f2f042d90388909f8b
Kel Strother <kelstrother@gmail.com>
2023-03-26 13:49:59 -0600

    created a reusable service card component and imported into service scroller container.

---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="service-box">
+    <h2 class="service-name">{{ heading }}</h2>
+    <slot></slot>
+    <p class="service-description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+import '@/components/ServiceCard.css'
+export default {
+  name: "ServiceCard",
+  props: ["heading", "description"]
+};
+</script>
--- src/components/ServiceScroller.vue
+++ src/components/ServiceScroller.vue
@@ -10,52 +10,35 @@
     </h2>
     <section class="service-scroller">
       <div class="service-box-container snaps-inline">
-        <article
-          class="service-box"
+        <ServiceCard
           data-aos="fade-up"
           data-aos-duration="800"
           data-aos-easing="ease-in-sine"
+          heading="Taxes"
+          description="Whether you're an individual or a business, we have the expertise to help you navigate the complexities of tax laws and regulations."
         >
-          <h2 class="service-name">Tax Preperation</h2>
-          <div class="icon-container">
-          <MoneyIcon class="icon" />
-          </div>
-          <p class="service-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea illo
-            fugit modi esse praesentium voluptate exercitationem saepe sapiente
-            veritatis.
-          </p>
-        </article>
-        <article
-          class="service-box"
+          <g-image class="service-img" src="../assets/images/taxes.jpg" />
+        </ServiceCard>
+        <ServiceCard
           data-aos="fade-up"
           data-aos-duration="800"
           data-aos-easing="ease-in-sine"
           data-aos-delay="100"
+          heading="Strategic Planning"
+          description="We provide peace of mind for you and your business. Whether it's seeking to make informed decisions about investments and financing or other strategic initiatives, we've got you covered."
         >
-          <h2 class="service-name">Tax Planning</h2>
-          <FinanceIcon class="icon" />
-          <p class="service-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-            voluptas quia recusandae ipsa aperiam neque aliquam dicta, nesciunt
-            praesentium.
-          </p>
-        </article>
-        <article
-          class="service-box"
+          <g-image class="service-img" src="../assets/images/planning.jpg" />
+        </ServiceCard>
+        <ServiceCard
           data-aos="fade-up"
           data-aos-duration="800"
           data-aos-easing="ease-in-sine"
           data-aos-delay="200"
+          heading="Business Plans"
+          description="Transform your business with our expert advice that will help you streamline operations, increase profitability, and achieve your financial goals."
         >
-          <h2 class="service-name">Business Consulting</h2>
-          <BriefcaseIcon class="icon" />
-          <p class="service-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-            officia alias distinctio laborum? Quibusdam, exercitationem. Optio,
-            quidem fuga.
-          </p>
-        </article>
+          <g-image class="service-img" src="../assets/images/consulting.jpg" />
+        </ServiceCard>
       </div>
     </section>
     <div class="service-link-container">
@@ -72,15 +55,14 @@
 </template>
 <script>
 import "~/components/ServiceScroller.css";
+import ServiceCard from "../components/ServiceCard.vue";
 import FinanceIcon from "~/components/FinanceIcon.vue";
 import MoneyIcon from "~/components/MoneyIcon.vue";
 import BriefcaseIcon from "~/components/BriefcaseIcon.vue";
 export default {
   name: "ServiceScroller",
   components: {
-    FinanceIcon,
-    MoneyIcon,
-    BriefcaseIcon,
+    ServiceCard,
   },
 };
 </script>
